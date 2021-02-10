73b75d32b0d6087450f8ed80e5ddf2cb61100f0c
antonagre <antonagre03@gmail.com>
2021-02-11 00:47:02 +0100

    Update RunCucumberTest.java

--- src/test/java/org/calc/RunCucumberTest.java
+++ src/test/java/org/calc/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@Cucumber.Options(format = {"json:target/cucumber.json"})
 public class RunCucumberTest {
 
 }
